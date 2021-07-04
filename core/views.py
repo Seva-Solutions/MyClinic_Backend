13858d6ea242ef4c13e79b3c020322ed800cb53e
nandu pokhrel <nandupokhrel@gmail.com>
2021-07-03 23:24:05 -0400

    Adding enpoints to return total number of clinics,patients and doctors

--- core/views.py
+++ core/views.py
@@ -2,20 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from .management.commands.data import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-
-# Create your views here.
+@api_view(['GET'])
 def patient_view(request):
-    return JsonResponse({'patients': patients})
+    return JsonResponse(len(patients), safe=False)
 
-
+@api_view(['GET'])
 def doctor_view(request):
-    return JsonResponse({'doctors': doctors})
-
-
+    return JsonResponse(len(doctors), safe=False)
+    
+@api_view(['GET'])
 def clinic_view(request):
-    return JsonResponse({'clinics': clinics})
-
+    return JsonResponse(len(clinics), safe=False)
 
 def appointment_view(request):
     return JsonResponse({'appointments': appointments})
+
+
+
+
