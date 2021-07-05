dd87f0877c5043d00b3b5301f65d8dc7a5d4fa2e
Sailesh Sharma <sailesh@Saileshs-MacBook-Air.local>
2021-07-05 15:52:26 -0400

    fixed models

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from datetime import datetime, date
+from django.conf.global_settings import LANGUAGES
 
 # Create your models here.
 class Patient(models.Model):
-    ohip_id = models.CharField(max_length=15, primary_key=True)
+    id = models.CharField(max_length=50,primary_key=True,default='')
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
     dob = models.DateField()
@@ -10,55 +12,49 @@ class Patient(models.Model):
     gender = models.CharField(max_length=6)
     email = models.EmailField(default=None)
     isEmailVerified = models.BooleanField(default=False)
+    insuranceNumber = models.CharField(max_length=30)
+    isInsuranceNumberVerified = models.BooleanField(default=False)
     phoneNumber = models.CharField(max_length=15, default='')
     image = models.CharField(max_length=100, default='')
-    clinic_id = models.ForeignKey(
-        'Clinic',
-        on_delete=models.CASCADE,
-    )
-
     class Meta:
         ordering = ['ohip_id']
 
 
 class Doctor(models.Model):
-    cpso_id = models.IntegerField(primary_key=True)
+    id = models.CharField(max_length=50,primary_key=True,default='')
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
+    doctor_license = models.CharField(max_length=30)
+    isLicenseVerified = models.BooleanField(default=False)
     specialty = models.CharField(max_length=50)
     gender = models.CharField(max_length=6)
-    clinic_id = models.ForeignKey(
-        'Clinic',
-        on_delete=models.CASCADE,
-    )
 
     class Meta:
         ordering = ['cpso_id']
 
 
 class Clinic(models.Model):
-    clinic_id = models.IntegerField(primary_key=True)
+    id = models.CharField(max_length=50,primary_key=True,default='')
     name = models.CharField(max_length=100)
     days = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
+    clinic_license = models.CharField(max_length=30)
+    isLicenseVerified = models.BooleanField(default=False)
+    password = models.CharField(max_length=30)
 
     class Meta:
         ordering = ['clinic_id']
 
 
 class Appointment(models.Model):
-    clinic_id = models.ForeignKey(
-        'Clinic',
-        on_delete=models.CASCADE,
-    )
-    cpso_id = models.ForeignKey(
-        'Doctor',
-        on_delete=models.CASCADE,
-    )
-    ohip_id = models.ForeignKey(
-        'Patient',
-        on_delete=models.CASCADE,
-    )
+    id = models.CharField(max_length=50,primary_key=True,default='')
+    startTime = models.DateField()
+    endTime = models.DateField()
+    doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE,default=None)
+    clinic_id = models.ForeignKey(Clinic, on_delete=models.CASCADE,default=None)
+    patient_id = models.ForeignKey(Patient, on_delete=models.CASCADE,default=None)
+    appointment_type = models.CharField(max_length=100)
+
 
     class Types(models.TextChoices):
         full_checkup = "full checkup"
@@ -67,14 +63,6 @@ class Appointment(models.Model):
         prescription_refill = "prescription refill"
         general_consultation = "general consultation"
         other = "other"
-
-    appointment_type = models.CharField(max_length=30, choices=Types.choices)
-    start_time = models.CharField(max_length=10)
-    end_time = models.CharField(max_length=10)
-    date = models.DateField()
-    duration = models.IntegerField(default=0)
-    address = models.CharField(max_length=100, default="")
-
     class Meta:
         ordering = ['id']
 
