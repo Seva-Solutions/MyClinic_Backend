794b2adf566414ac2b0535e7f57d6802b42d6a58
nandu pokhrel <nandupokhrel@gmail.com>
2021-07-05 12:06:38 -0400

    adding checks to the endpoint

--- core/views.py
+++ core/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.http.request import HttpRequest
 from .management.commands.data import *
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
+
 
 @api_view(['GET'])
 def patient_view(request):
-    return JsonResponse(len(patients), safe=False)
+    if request.method == "GET":
+        return JsonResponse(len(patients), safe=False)
 
 @api_view(['GET'])
 def doctor_view(request):
-    return JsonResponse(len(doctors), safe=False)
+    if request.method == "GET":
+        return JsonResponse(len(doctors), safe=False)
     
 @api_view(['GET'])
 def clinic_view(request):
-    return JsonResponse(len(clinics), safe=False)
+    if request.method == "GET":
+        return JsonResponse(len(clinics), safe=False)
 
 def appointment_view(request):
     return JsonResponse({'appointments': appointments})
