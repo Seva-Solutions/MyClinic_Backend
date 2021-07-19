3da2a056a0f6509388a34dea028ee6ba4fa897a4
Sailesh Sharma <sailesh@Saileshs-MacBook-Air.local>
2021-07-19 16:39:48 -0400

    added path for nearby clinics

--- Atreya/clinics/urls.py
+++ Atreya/clinics/urls.py
@@ -4,7 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('clinics/total_clinics', views.total_clinics, name='total-clinics'),
-    path('clinics/<int:clinic_id>',views.clinics),  
+    path('clinics/<int:clinic_id>',views.clinics),
+    path('clinics/get_nearby_clinics/<id>', nearby_clinics),  
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
